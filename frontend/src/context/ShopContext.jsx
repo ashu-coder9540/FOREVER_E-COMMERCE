286b4c40943f488ca3fd3b1c2888818f978f466b
ashu-coder9540 <ashu954008@gmail.com>
2026-07-26 16:12:16 +0530

    Implement cart functionality with addToCart and updateQuantity methods, enhance Cart and Product components, and integrate Toast notifications for error handling.

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -9,10 +10,66 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemId, size) => {
+
+        if(!size){
+            toast.error('Select Product Size');
+            return;
+        }
+
+        let cartData = structuredClone(cartItems);
+        
+        if(cartData[itemId]){ //if product exists
+            if(cartData[itemId][size]){ // check if size exists → increase quantity
+                cartData[itemId][size] += 1;
+            }
+            else{ //if not exist add size with quantity 1
+                cartData[itemId][size] = 1;
+            }
+        }
+        else{ //if product didn't exists
+            cartData[itemId] = {}; //create empty object of product
+            cartData[itemId][size] = 1; //add size entry with quantity 1
+        }
+        setCartItems(cartData);
+    }
+
+    useEffect(()=>{
+        console.log(cartItems)
+    },[cartItems])
+
+    const getCartCount = () => {
+        let totalCount = 0;
+        for(const items in cartItems){
+            for(const item in cartItems[items]){
+                try{
+                    if(cartItems[items][item] > 0){
+                        totalCount += cartItems[items][item];
+                    }
+                }catch(err){
+
+                }
+            }
+        }
+        return totalCount;
+    }
+
+    const updateQuantity = async (itemId, size, quantity) => {
+
+        let cartData = structuredClone(cartItems);
+
+        cartData[itemId][size] = quantity;
+
+        setCartItems(cartData);
+    }
 
     const value = {
         products, currency, delivery_fee,
-        search, setSearch, showSearch, setShowSearch
+        search, setSearch, showSearch, setShowSearch,
+        cartItems, addToCart,
+        getCartCount, updateQuantity
     }
 
     return (
